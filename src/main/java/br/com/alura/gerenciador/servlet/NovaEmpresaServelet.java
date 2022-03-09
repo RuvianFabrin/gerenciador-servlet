fa1a671ef4eb20b711d264e80c81cf894120d815
Ruvian Fabrin <ruvian.fabrin@hotmail.com>
2022-03-08 22:23:48 -0300

    Feito Get e post

---
+++ src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServelet.java
@@ -0,0 +1,41 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class NovaEmpresaServelet
+ */
+@WebServlet("/novaEmpresa")
+public class NovaEmpresaServelet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NovaEmpresaServelet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Cadastrando nova empresa");
+		String nomeEmp = request.getParameter("nome");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Cadastrando a empresa: "+nomeEmp);
+		out.println("</body>");
+		out.println("<html>");
+	}
+
+}
